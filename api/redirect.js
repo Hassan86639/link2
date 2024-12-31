47042073d8cea09a9e35732e0f8eeec5dc591841
Hassan86639 <hassan86639@gmail.com>
2024-12-31 20:44:46 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://pinkpetals.pk/collections/new-arrivals/products/amber-gold?variant=46468169203935";
+    const whitePageURL = "https://www.cheapaschips.com.au/products/c1004938";
     const blackPageURL = "https://cggkidswear.myfunnelish.com/super-active-toddler-jacket";
   
     // Parse the UTM parameters from the request URL
